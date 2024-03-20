99814a57ef885325d142e5744b1570201cf65a3e
David Maldonado <david.maldonado.4810@gmail.com>
2024-03-19 23:00:52 -0600

    Version inicial

--- src/main/java/com/example/lehmer/Lehmer.java
+++ src/main/java/com/example/lehmer/Lehmer.java
@@ -20,23 +20,29 @@ public class Lehmer {
 
     @FXML private TableView<Numero> table;
     @FXML private TableColumn<Numero , Integer> iteracionCol;
+    @FXML private TableColumn<Numero , Long> YCol;
+    @FXML private TableColumn<Numero , Long> Extrac;
     @FXML private TableColumn<Numero , Double> pseudoCol;
+    @FXML private TableColumn<Numero , Long> ZCol;
 
     private long ExtracLong;
     private double m;
     private long A;
     private int num;
     private long z;
+    private long z2;
     private long Ylong;
 
-    public Lehmer(long Xo, long A, long ExtracLong, double m, int num, long z, long Ylong) {
+    public Lehmer(long Xo, long A, long ExtracLong, double m, int num, long z, long z2, long Ylong) {
         this.Xo = Xo;
         this.A = A;
         this.ExtracLong = ExtracLong;
         this.m = m;
         this.num = num;
         this.z = z;
+        this.z2 = z2;
         this.Ylong = Ylong;
+
     }
 
     public double next() {
@@ -44,24 +50,28 @@ public class Lehmer {
         int largo = mString.length();
 
         double m = Math.pow(10, largo);
-        long z = A*Xo;
+        z = A*Xo;
         String ZString = Long.toString(z);
         int ZLong = ZString.length();
 
         String parteExtraida = ZString.substring(0, ZLong-largo);
-        Long ExtracLong = Long.parseLong(parteExtraida);
+        ExtracLong = Long.parseLong(parteExtraida);
 
         int diferencia = ZString.length() - largo;
         String YString = ZString.substring(diferencia, ZLong);
-        Long Ylong = Long.parseLong(YString);
+        Ylong = Long.parseLong(YString);
 
         Xo = (Ylong - ExtracLong);
+        z2=A*Xo;
         return (double) Xo / m;
     }
 
     public void initialize(){
         iteracionCol.setCellValueFactory(new PropertyValueFactory<Numero, Integer>("iteracion"));
+        YCol.setCellValueFactory(new PropertyValueFactory<Numero, Long>("YCol"));
+        Extrac.setCellValueFactory(new PropertyValueFactory<Numero, Long>("Extrac"));
         pseudoCol.setCellValueFactory(new PropertyValueFactory<Numero, Double>("pseudoNumero"));
+        ZCol.setCellValueFactory(new PropertyValueFactory<Numero, Long>("ZCol"));
 
         UnaryOperator<TextFormatter.Change> filter = change -> {
             String text = change.getText();
@@ -77,11 +87,13 @@ public class Lehmer {
 
 
     public void Aceptar(){
-        long Xo = Long.parseLong(XoText.getText());
+        this.table.getItems().clear();
 
-        long A = Long.parseLong(AText.getText());
+        long Xo = DatoXo();
 
-        int num = Integer.parseInt(numText.getText());
+        long A = DatoA();
+
+        int num = DatoNum();
 
         String mString = Long.toString(Xo);
         int largo = mString.length();
@@ -98,14 +110,52 @@ public class Lehmer {
         String YString = ZString.substring(diferencia, ZLong);
         Long Ylong = Long.parseLong(YString);
 
-        Lehmer rng = new Lehmer(Xo, A, ExtracLong, m, num, z, Ylong);
+        Lehmer rng = new Lehmer(Xo, A, 0, 0, num, 0, 0,0);
 
         // Generar y mostrar algunos números pseudoaleatorios
         for (int i = 1; i <= num; i++) {
 
-            Numero numero = new Numero (i, rng.next());
+            Numero numero = new Numero (i, rng.next(), rng.getYlong(), rng.getExtracLong(), rng.getZ2());
             this.table.getItems().add(numero);
         }
 
+    }
+
+    public long DatoXo(){
+        if(XoText.getText() != ""){
+            long Dato1 = Long.parseLong(XoText.getText());
+            return Dato1;
+        } else {
+            throw new IllegalArgumentException("Faltan datos en Xo.");
+        }
+    }
+
+    public long DatoA(){
+        if(AText.getText() != ""){
+            long Dato2 = Long.parseLong(AText.getText());
+            return Dato2;
+        } else {
+            throw new IllegalArgumentException("Faltan datos en A.");
+        }
+    }
+
+    public int DatoNum(){
+        if(numText.getText() != ""){
+            int Dato3 = Integer.parseInt(numText.getText());
+            return Dato3;
+        } else {
+            throw new IllegalArgumentException("Faltan datos en Numero de iteraciones.");
+        }
+    }
+    public long getYlong() {
+        return Ylong;
+    }
+
+    public long getExtracLong() {
+        return ExtracLong;
+    }
+
+    public long getZ2() {
+        return z2;
     }
 }
--- src/main/java/com/example/lehmer/Numero.java
+++ src/main/java/com/example/lehmer/Numero.java
@@ -1,12 +1,20 @@
 package com.example.lehmer;
 
+import java.security.PrivateKey;
+
 public class Numero {
     private int iteracion;
     private double pseudoNumero;
+    private long YCol;
+    private long Extrac;
+    private long ZCol;
 
-    public Numero(int iteracion, double pseudoNumero) {
+    public Numero(int iteracion, double pseudoNumero, long YCol, long Extrac, long ZCol) {
         this.iteracion = iteracion;
         this.pseudoNumero = pseudoNumero;
+        this.YCol = YCol;
+        this.Extrac = Extrac;
+        this.ZCol = ZCol;
     }
 
     public int getIteracion() {
@@ -23,5 +31,26 @@ public class Numero {
 
     public void setPseudoNumero(double pseudoNumero) {
         this.pseudoNumero = pseudoNumero;
+    }
+    public long getYCol() {
+        return YCol;
+    }
+    public void setY(long YCol) {
+        this.YCol = YCol;
+    }
+
+    public long getExtrac() {
+        return Extrac;
+    }
+    public void setExtrac(long Extrac) {
+        this.Extrac = Extrac;
+    }
+
+    public long getZCol() {
+        return ZCol;
+    }
+
+    public void setZCol(long Z) {
+        this.ZCol = ZCol;
     }
 }
